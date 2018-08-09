d5921265d2a4886e049fe95eaf54d4414dab402c
7zwei <styling777@ukr.net>
2018-08-09 20:04:12 +0300

    single query scrape and write

--- MainScraper.py
+++ MainScraper.py
@@ -3,15 +3,19 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from itertools import cycle
 from time import sleep
+from user_agent import generate_user_agent
+import re
+import csv
 
 
 proxies = {
     'https': '80.188.10.50:8080'
 }
-proxy_pool = cycle(proxies)
+headers = {'User-Agent': generate_user_agent(device_type="all", navigator=('chrome', 'firefox', 'ie'),
+                                             os=('mac', 'linux'))}
 
 
-def get_search_url(search_query, language, region, domain, number_of_results):
+def get_single_query_set(search_query, language, region, domain, number_of_results):
     assert isinstance(search_query, str), "Search query must be a string."
     assert isinstance(number_of_results, int) and 10 <= number_of_results <= 100,\
         "Number of results must be an integer in range 10:100."
@@ -21,31 +25,20 @@ def get_search_url(search_query, language, region, domain, number_of_results):
     google_search_query = search_query.replace(' ', '+')
     search_url = 'https://{}/search?q={}&num={}&hl={}&gl={}'.format(domain, google_search_query, number_of_results,
                                                                     language, region)
-    return search_url
+    query_set = [search_query]
+    page_response = requests.get(search_url, timeout=5)
+    soup = BeautifulSoup(page_response.content, "html5lib")
+    for i in soup.findAll("h3", attrs={'class':'r'}):
+        query_set.append(i.find('a')['href'].split('=')[1])
+    return query_set
 
 
-def fetch_results(search_url):
-    driver = webdriver.Chrome()
-    driver.get(search_url)
-    sleep(100)
-    driver.find_element_by_xpath('//*[@id="rso"]/div[2]/div/div[2]/div/div/h3/a')
-    #"//*[@id="rso"]/div[4]/div/div[19]/div/div/h3/a"
-    #"//*[@id="rso"]/div[4]/div/div[11]/div/div/h3/a"
-    #"//*[@id="rso"]/div[2]/div/div[1]/div/div/h3/a"
-    #"//*[@id="rso"]/div[2]/div/div[2]/div/div/h3/a"
-    #"//*[@id="rso"]/div[4]/div/div[44]/div/div/h3/a"
-
-
-def test_proxies():
-    for i in range(0,10):
-        proxy = next(proxy_pool)
-        print("request #%d"%i)
-        try:
-            response = requests.get("https://www.google.com.ua/search?q=sanya", proxies={"http": proxy, "https": proxy})
-            print(response.json())
-        except:
-            print("Sss")
+def write_into_tsv(query_set):
+    with open('output.tsv', 'a') as tsvfile:
+        writer = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
+        writer.writerow(query_set)
 
 
 if __name__ == "__main__":
-    fetch_results(get_search_url('sanya beliy', 'ru', 'ua', 'www.google.com.ua', 50))
+    q = get_single_query_set('dima', 'ru', 'ua', 'www.google.com.ua', 10)
+    write_into_tsv(q)
